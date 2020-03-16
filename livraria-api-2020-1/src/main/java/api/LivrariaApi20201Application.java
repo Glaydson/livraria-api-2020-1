4fa149e5ab3fbf00274715a8daad9f12ca60791c
glaydson <glaydsonvs@gmail.com>
2020-03-16 13:57:33 -0300

    Inicializando a aplicação

---
+++ livraria-api-2020-1/src/main/java/api/LivrariaApi20201Application.java
@@ -0,0 +1,13 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaApi20201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaApi20201Application.class, args);
+	}
+
+}
