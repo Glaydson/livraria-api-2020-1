294661d2509e2c102ce51a8288435b27f0981a20
glaydson <glaydsonvs@gmail.com>
2020-04-14 21:36:31 -0300

    Exercícios da aula de 14-04-2020 - consultas derivadas

--- livraria-api-2020-1/src/main/java/api/repositorios/AutorRepository.java
+++ livraria-api-2020-1/src/main/java/api/repositorios/AutorRepository.java
@@ -1,9 +1,15 @@
 package api.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import api.entidades.Autor;
+import api.entidades.Livro;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+	
+	// Todos os autores de um dado livro
+	List<Autor> findByLivrosTitulo(String tituloLivro);
 
 }
