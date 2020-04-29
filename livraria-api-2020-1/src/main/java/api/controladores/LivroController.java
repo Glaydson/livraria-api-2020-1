e1a2a54b46cac81754c5f9b0a57ac77eda5ccc09
glaydson <glaydsonvs@gmail.com>
2020-04-28 22:36:23 -0300

    Aula de 28-04-2020

--- livraria-api-2020-1/src/main/java/api/controladores/LivroController.java
+++ livraria-api-2020-1/src/main/java/api/controladores/LivroController.java
@@ -1,21 +1,26 @@
 package api.controladores;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import api.entidades.Livro;
+import api.excecoes.LivroNaoEncontradoException;
 import api.servicos.LivroService;
 
 @RestController
@@ -48,5 +53,35 @@ public class LivroController {
 				.buildAndExpand(livroSalvo.getLivroID()).toUri();
 		return ResponseEntity.created(local).build();
 	}
+
+	@PutMapping("/atualiza/{id}")
+	public ResponseEntity<Object> atualizaLivro(@RequestBody Livro livro, @PathVariable long id) {
+		try {
+			Livro livroBD = servicoLivros.buscarPeloID(id);
+		} catch (LivroNaoEncontradoException lnee) {
+			return ResponseEntity.notFound().build();
+		}
+		livro.setLivroID(id);
+		servicoLivros.salvar(livro);
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/numeroPaginas/maior")
+	public List<Livro> buscarPeloNumeroPaginasMaior(@RequestParam int numeroPaginas) {
+		return this.servicoLivros.buscarPeloNumeroDePaginasMaiorQue(numeroPaginas);
+	}
+	
+	@GetMapping("/numeroPaginas/entre/{min}/{max}")
+	public List<Livro> buscarPeloNumeroPaginasIntervalo(@PathVariable int min, @PathVariable int max) {
+		return this.servicoLivros.buscarPeloNumeroDePaginasIntervalo(min, max);
+	}
+
+	@GetMapping("/dataPublicacaoEntre")
+	public List<Livro> buscarPorDataPublicacaoEntre(
+		@RequestParam @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dataInicial,
+		@RequestParam @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dataFinal) {
+	     return this.servicoLivros.buscarDataPublicacaoEntre(dataInicial, dataFinal);
+	}
+
 
 }
--- livraria-api-2020-1/src/main/java/api/excecoes/LivroNaoEncontradoException.java
+++ livraria-api-2020-1/src/main/java/api/excecoes/LivroNaoEncontradoException.java
@@ -1,6 +1,11 @@
 package api.excecoes;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class LivroNaoEncontradoException extends RuntimeException {
+	
 	
 	public LivroNaoEncontradoException(Long id) {
 		super("Livro com o id " + id + " não encontrado.");
